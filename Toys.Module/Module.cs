15adb80f436ed9103a97e3dc2b4302f9be17acb6
Kirsten Greed <kirsteng@jobtalk.com.au>
2021-03-06 09:59:35 +1100

    start

--- Toys.Module/Module.cs
+++ Toys.Module/Module.cs
@@ -12,7 +12,7 @@ using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Updating;
 using DevExpress.ExpressApp.Model.Core;
 using DevExpress.ExpressApp.Model.DomainLogics;
-using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.Model.NodeGenerators; 
 using System.Data.Entity;
 using Toys.Module.BusinessObjects;
 
@@ -34,7 +34,7 @@ namespace Toys.Module {
         }
         public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
             ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
-            return new ModuleUpdater[] { updater };
+            return new ModuleUpdater[] { updater }; 
         }
         public override void Setup(XafApplication application) {
             base.Setup(application);
