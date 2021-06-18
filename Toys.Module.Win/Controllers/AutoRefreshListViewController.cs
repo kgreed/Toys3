4244280910e7b898001e671922fea350917f9872
Kirsten Greed <kirsteng@jobtalk.com.au>
2021-06-18 11:17:44 +1000

    T1007234

---
+++ Toys.Module.Win/Controllers/AutoRefreshListViewController.cs
@@ -0,0 +1,44 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.ExpressApp.Win.Editors;
+using Toys.Module.BusinessObjects;
+namespace Toys.Module.Win.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+     
+        public partial class AutoRefreshListViewController : ObjectViewController<ListView, BaseNonPersistent>
+        {
+            protected override void OnActivated()
+            {
+                base.OnActivated();
+                ObjectSpace.ObjectChanged += ObjectSpace_ObjectChanged;
+            }
+
+            protected override void OnDeactivated()
+            {
+                ObjectSpace.ObjectChanged -= ObjectSpace_ObjectChanged;
+                base.OnDeactivated();
+            }
+
+            private void ObjectSpace_ObjectChanged(object sender, ObjectChangedEventArgs e)
+            {
+                GridListEditor editor = View.Editor as GridListEditor;
+                if (editor != null && editor.GridView != null && editor.GridView.FocusedRowHandle >= 0)
+                    editor.GridView.RefreshRow(editor.GridView.FocusedRowHandle);
+            }
+        }
+    
+}
--- Toys.Module/NonPersistentObjects/NonPersistentDetailView.cs
+++ Toys.Module/NonPersistentObjects/NonPersistentDetailView.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using DevExpress.ExpressApp;
-using DevExpress.ExpressApp.Editors;
-using Toys.Module.BusinessObjects;
-namespace Toys.Module.NonPersistentObjects
-{
-    public partial class NonPersistentDetailView : ObjectViewController<DetailView, BaseNonPersistent>
-    {
-        protected override void OnActivated()
-        {
-            base.OnActivated();
-            View.CustomizeViewItemControl(this, SetViewItem);
-        }
+﻿//using System;
+//using DevExpress.ExpressApp;
+//using DevExpress.ExpressApp.Editors;
+//using Toys.Module.BusinessObjects;
+//namespace Toys.Module.NonPersistentObjects
+//{
+//    public partial class NonPersistentDetailView : ObjectViewController<DetailView, BaseNonPersistent>
+//    {
+//        protected override void OnActivated()
+//        {
+//            base.OnActivated();
+//            View.CustomizeViewItemControl(this, SetViewItem);
+//        }
 
-        private void SetViewItem(ViewItem viewItem)
-        {
-            var ed = viewItem as PropertyEditor;
-            ed.ValueStored += Ed_ValueStored;
-        }
+//        private void SetViewItem(ViewItem viewItem)
+//        {
+//            var ed = viewItem as PropertyEditor;
+//            ed.ValueStored += Ed_ValueStored;
+//        }
 
-        private void Ed_ValueStored(object sender, EventArgs e)
-        {
-           View.ObjectSpace.SetModified(View.CurrentObject);
-        }
-    }
-}
+//        private void Ed_ValueStored(object sender, EventArgs e)
+//        {
+//            //View.ObjectSpace.SetModified(View.CurrentObject);
+//        }
+//    }
+//}
